3575d8cf6ceb738583706560e621a0a1e6f814fe
Clayton Wieberg <55328479+klayza@users.noreply.github.com>
2025-08-09 14:41:43 -0700

    refactor

---
+++ README.md
@@ -0,0 +1,5 @@
+## Description
+Ever wanted to create your own life?
+
+With this you can simulate a few cells and watch them form new fantastic shapes! Cells can merge together to form spaceships, spinners, lasers, or even a computer!
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import time
 import pygame
 
 # Pygame init
@@ -7,13 +8,19 @@ screen_w = 720
 screen_h = 720
 screen = pygame.display.set_mode((screen_w, screen_h))
 clock = pygame.time.Clock()
+font = pygame.font.SysFont("Consolas", 20)
 running = True
 
+
 # Game variables
+DEAD_CELL = 0
+ALIVE_CELL = 1
 board_size = 50
-board = [[0 for _ in range(board_size)] for _ in range(board_size)]
-tile_size = math.sqrt((screen_w * screen_h) / math.pow(board_size, 2))
+board = [[DEAD_CELL for _ in range(board_size)] for _ in range(board_size)]
+tile_size = math.floor(math.sqrt((screen_w * screen_h) / math.pow(board_size, 2)))
+print(tile_size)
 walked_over = [[0 for _ in range(board_size)] for _ in range(board_size)]
+generation = 0
 
 species = {
     # Still Lifes (static patterns)
@@ -35,7 +42,10 @@ species = {
 
 def draw_species(board, species_name, x, y):
     for sx, sy in species[species_name]:
-        board[y + sy][x + sx] = 1
+        # Check for species spawned outside of board
+        if sx + x >= board_size or sy + y >= board_size: 
+            return
+        board[y + sy][x + sx] = ALIVE_CELL
 
 
 def draw_board(board):
@@ -55,8 +65,21 @@ def draw_game():
     board = next_generation(board)
 
 
-def next_generation(current_board):
+def draw_scene(name="default"):
+    if name == "default":
+        draw_species(board, "glider", 2, 2)
+        draw_species(board, "blinker", 15, 5)
+        draw_species(board, "toad", 25, 8)
+        draw_species(board, "beacon", 35, 12)
+        draw_species(board, "loaf", 10, 20)
+        draw_species(board, "beehive", 25, 30)
+        draw_species(board, "block", 40, 40)
 
+
+
+def next_generation(current_board):
+    global generation
+    generation += 1
     new_board = [[cell for cell in row] for row in current_board]
 
     for y, row in enumerate(current_board):
@@ -68,18 +91,18 @@ def next_generation(current_board):
 
             # 1. If live cell has < 2 live neighbors: dead
             if alive and neighbors < 2:
-                new_board[y][x] = 0
+                new_board[y][x] = DEAD_CELL
 
             # 2. If live cell has 2 or 3 live neighbors: continue
 
 
             # 3. If live cell has > 3 live neighbors: dead
             if alive and neighbors > 3:
-                new_board[y][x] = 0
+                new_board[y][x] = DEAD_CELL
 
             # 4. If dead cell has = 3 live neighbors: alive
             if not alive and neighbors == 3: 
-                new_board[y][x] = 1
+                new_board[y][x] = ALIVE_CELL
 
     return new_board
 
@@ -104,28 +127,40 @@ def game_loop():
     global running
     global board
 
-    draw_species(board, "glider", 2, 2)
-    draw_species(board, "blinker", 15, 5)
-    draw_species(board, "toad", 25, 8)
-    draw_species(board, "beacon", 35, 12)
-    draw_species(board, "loaf", 10, 20)
-    draw_species(board, "beehive", 25, 30)
-    draw_species(board, "block", 40, 40)
+    draw_scene()
 
+    paused = False
     while running:
-    # Check if the game is closed
+        # Check if the game is closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+
+        # Check keys
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            paused = not paused
+
+
         # Set color
         screen.fill("white")
 
         # Render game
-        draw_game()
+        if not paused:
+            draw_game()
+        else:
+            draw_board(board)
+
+
+        # Render debug info
+        debug_text = font.render('Paused' if paused else 'Paused', False, (0, 0, 0))
+        gen_text = font.render('Generation ' + str(generation), False, (0, 0, 0))
+        screen.blit(debug_text, (10,10))
+        screen.blit(gen_text, (100,10))
 
         # Update screen
         pygame.display.flip()
+            
         clock.tick(20) # limit fps to 60
 
 
